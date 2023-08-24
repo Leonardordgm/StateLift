a60f3672f73532a2988175ed4ebb3d71c6cee8a2
Leonardo Rodrigues Martins <117056433+Leonardordgm@users.noreply.github.com>
2023-08-23 22:18:09 -0300

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import SeuNome from './evento/SeuNome';
+import { useState } from 'react';
+import Saudacao from './evento/Saudacao';
+
+function App() {
+
+  const [nome, setNome] = useState() 
+
+
+  return (
+    <div className='App'>
+      <h1>State Lift</h1>
+      <SeuNome setNome={setNome}/>
+      <Saudacao nome={nome}/>  
+    </div>
+  );
+}
+
+export default App;
